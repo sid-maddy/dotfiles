b7bed9966b75c43f1733d43c9b78f11618819594
Siddhesh Mhadnak <siddheshmhadnak@gofynd.com>
2026-01-01 12:52:57 +0530

    chore: jan 2026 updates

--- private_dot_config/nvim/lua/plugins/productivity.lua
+++ private_dot_config/nvim/lua/plugins/productivity.lua
@@ -1,30 +1,29 @@
 return {
-  {
-    'nvim-neorg/neorg',
-    dependencies = { 'benlubas/neorg-interim-ls' },
-    lazy = false,
-    version = false,
-    -- ft = 'norg',
-    opts = {
-      load = {
-        ['core.completion'] = { config = { engine = { module_name = 'external.lsp-completion' } } },
-        ['core.concealer'] = {},
-        ['core.defaults'] = {},
-        ['core.dirman'] = {
-          config = {
-            workspaces = {
-              docs = '~/Documents',
-              work_docs = '~/Documents/work/docs',
-              work_tasks = '~/Documents/work/tasks',
-            },
-          },
-        },
-        -- ["core.export"] = {},
-        ['core.integrations.treesitter'] = {},
-        ['core.journal'] = { config = { workspace = 'docs' } },
-        ['core.presenter'] = { config = { zen_mode = 'zen-mode' } },
-      },
-    },
-    build = ':Neorg sync-parsers',
-  },
+	{
+		"nvim-neorg/neorg",
+		dependencies = { "benlubas/neorg-interim-ls" },
+		lazy = false,
+		version = "*",
+		opts = {
+			load = {
+				["core.completion"] = { config = { engine = { module_name = "external.lsp-completion" } } },
+				["core.concealer"] = {},
+				["core.defaults"] = {},
+				["core.dirman"] = {
+					config = {
+						workspaces = {
+							docs = "~/Documents",
+							work_docs = "~/Documents/work/docs",
+							work_tasks = "~/Documents/work/tasks",
+						},
+					},
+				},
+				-- ["core.export"] = {},
+				["core.integrations.treesitter"] = {},
+				["core.journal"] = { config = { workspace = "docs" } },
+				["core.presenter"] = { config = { zen_mode = "zen-mode" } },
+			},
+		},
+		build = ":Neorg sync-parsers",
+	},
 }
