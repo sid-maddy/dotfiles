28fcd7ab652ee24dc4ca8d9e8d544be30b43bead
Siddhesh Mhadnak <siddhesh.mhadnak@outlook.com>
2026-02-02 11:33:57 +0530

    chore: feb 2026 updates

--- dot_local/share/aliases.zsh
+++ dot_local/share/aliases.zsh
@@ -10,17 +10,8 @@ alias gf='git fetch -f'
 alias gl='git pull -f'
 alias gmm='git merge $(git_main_branch)'
 
-alias gt='gitui'
-alias lg='lazygit'
-
 # NeoVim
 alias vimdiff='nvim -d'
-
-# Poetry
-alias prun='poetry run'
-alias pred="poetry run ${EDITOR}"
-alias pot='poetry task'
-alias prt='poetry run task'
 
 # UV
 alias uvrun='uv run'
